1ab739f780c5124d7e33484776fad4d83d62cc9c
Cyrille-18 <cyrillepio@hotmail.com>
2025-05-20 09:37:09 +0000

    Chore: Load environment variables and update connection string handling

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+using DotNetEnv;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+Env.Load(); // load environment variable in .env
+
+var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")
+    ?? throw new Exception("CONNECTION_STRING is missing in .env");
 
 var app = builder.Build();
 
@@ -23,14 +28,13 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast(
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
+            summaries[Random.Shared.Next(summaries.Length)]))
         .ToArray();
+
     return forecast;
 })
 .WithName("GetWeatherForecast")
