1ef9eda6f0338f101e32b72a1b3eba5099cf8f1b
Cyrille-18 <cyrillepio@hotmail.com>
2025-05-20 23:15:12 +0000

    Feat(Migration): Add CreateProductTable migration with Products table definition

---
+++ Migrations/20250520230814_CreateProductTable.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="20250520230814_CreateProductTable.cs" company="Maplenou">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+#nullable disable
+
+namespace MaplenouApi.Migrations
+{
+    using System;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <inheritdoc />
+    public partial class CreateProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    ImageUrl = table.Column<string>(type: "text", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
