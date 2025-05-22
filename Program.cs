63648c72a9e51aa9a687013e03b28e92d67527d0
Cyrille-18 <cyrillepio@hotmail.com>
2025-05-22 11:30:50 +0000

    Fix(Copyright): Update copyright information in Program.cs and remove unused weather forecast code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 // <copyright file="Program.cs" company="Maplenou">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+// Copyright © Maplenou 2025
 // </copyright>
 
 using DotNetEnv;
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 Env.Load(); // load environment variable in .env
 
 var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING")
@@ -32,28 +33,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast(
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]))
-        .ToArray();
-
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
