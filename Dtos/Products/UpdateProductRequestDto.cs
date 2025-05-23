7a5e6cd44ee1f16e15f84874d0c89e036f5001d0
Cyrille-18 <cyrillepio@hotmail.com>
2025-05-23 15:08:28 +0000

    Feat(UpdateProductRequestDto): Add DTO for updating product information

---
+++ Dtos/Products/UpdateProductRequestDto.cs
@@ -0,0 +1,42 @@
+// <copyright file="UpdateProductRequestDto.cs" company="Maplenou">
+// Copyright © Maplenou 2025
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaplenouApi.Dtos.Products
+{
+    /// <summary>
+    /// DTO for updating product information.
+    /// </summary>
+    public class UpdateProductRequestDto
+    {
+        /// <summary>
+        /// Gets or sets product title.
+        /// </summary>
+        public string Title { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the description of the product.
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the price the product.
+        /// </summary>
+        public float Price { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Quantity of the product.
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Gets or sets product image.
+        /// </summary>
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
