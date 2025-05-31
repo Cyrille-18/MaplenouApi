ddcc06ccd521bc3662880c661e4ad71f54ccead0
Cyrille-18 <cyrillepio@hotmail.com>
2025-05-31 12:44:13 +0000

    Feat(Migration): add SubcategoryId to Products and create Subcategories table with relationships

---
+++ Migrations/20250531102943_AddSubcategoryToProduct.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MaplenouApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSubcategoryToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "SubcategoryId",
+                table: "Products",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Subcategories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subcategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_SubcategoryId",
+                table: "Products",
+                column: "SubcategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Subcategories_SubcategoryId",
+                table: "Products",
+                column: "SubcategoryId",
+                principalTable: "Subcategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Subcategories_SubcategoryId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Subcategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_SubcategoryId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SubcategoryId",
+                table: "Products");
+        }
+    }
+}
