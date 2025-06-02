db3ed5afb4bbb802ae3cbe5fec6943f79d260b6b
Cyrille-18 <cyrillepio@hotmail.com>
2025-06-02 17:55:30 +0000

    Feat(Repository): add GetAllAsync method to ISubcategoryRepository for retrieving all subcategories with filtering and pagination

--- Interfaces/ISubcategoryRepository.cs
+++ Interfaces/ISubcategoryRepository.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MaplenouApi.Helpers;
 using MaplenouApi.Models;
 
 namespace MaplenouApi.Interfaces
@@ -28,5 +29,12 @@ namespace MaplenouApi.Interfaces
         /// <param name="id">The unique identifier of the subcategory.</param>
         /// <returns>The <see cref="Subcategory"/> if found; otherwise, <c>null</c>.</returns>
         Task<Subcategory?> GetByIdAsync(Guid id);
+
+        /// <summary>
+        /// Retrieves all subcategories asynchronously.
+        /// </summary>
+        /// <param name="queryObject">The query object containing filtering and pagination options for subcategories.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains a collection of <see cref="Subcategory"/> objects.</returns>
+        Task<List<Subcategory>> GetAllAsync(SubcategoryQueryObject queryObject);
     }
 }
